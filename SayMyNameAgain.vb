c7a05bae98ae3ac09d44a088846446de8b58ebf0
Laura <laurakriley01@gmail.com>
2021-01-26 22:15:01 -0700

    Add comments

--- SayMyNameAgain.vb
+++ SayMyNameAgain.vb
@@ -8,20 +8,25 @@ Option Strict On
 Option Explicit On
 
 'The program greets the user and gives instructions
-'has the user input their name and responds using their name
-Module SayMyName
-    
+'has the user input their name and responds using their name if their name is Emily or Joe
+Module SayMyNameAgain
+
     Sub Main()
         Dim userInput As String
 
+        'Prompts the user to write their name
         Console.WriteLine("Please type your name and press 'Enter'." & vbCrLf)
 
+        'Reads the users input
         userInput = Console.ReadLine()
 
+        'The program will respond with Emily or Joe if that is entered by the user
         If userInput = "Emily" Or userInput = "Joe" Then
             Console.WriteLine(vbCrLf & $"Hello {userInput}")
+            'If Laura is entered then it will ask the user the about Darth Plagueis The Wise
         ElseIf userInput = "Laura" Then
             Console.WriteLine(vbCrLf & $"Did you ever hear the tragedy of Darth Plagueis The Wise?")
+            'If Anything else is entered the program will respond saying they aren't authorized
         Else
             Console.WriteLine(vbCrLf & "You aren't authorized to read these files.")
         End If
